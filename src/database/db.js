1c6ce7aebaec2bfefe69558839f55b33be738608
Jose Eneas <ninho.serafim95@gmail.com>
2023-02-11 22:53:51 -0300

    teste 123

--- src/database/db.js
+++ src/database/db.js
@@ -7,6 +7,7 @@ const db = new Sequelize(
   {
     dialect: 'postgres',
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
     username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || '',
   }
